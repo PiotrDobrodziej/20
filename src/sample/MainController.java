99bf24941e6be8c9392c432fcc588fb2837c7c21
Oskar Pol <oskarbank@gmail.com>
2017-04-24 20:21:56 +0200

    asd

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -1,10 +1,16 @@
 package sample;
 
+import javafx.scene.input.MouseEvent;
+
 /**
  * Created by OskarPraca on 2017-04-24.
  */
 public class MainController {
     public void somebodyClickAnchor() {
+    }
 
+    public void clickSubmit(MouseEvent event)  {
+
+        System.out.println("Kliknięto mnie");
     }
 }
